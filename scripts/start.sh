f75e1e5430a71164c26b14c3719e3a1ae981e7b8
viniciusdornellas <vini_dcw@hotmail.com>
2023-12-20 17:01:38 -0300

    script

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,5 +4,9 @@ cd /appdir
 sudo npm cache clean -f
 sudo npm install
 
+process=`sudo lsof -i :9001`
+pid=`echo $process | grep -oP '\b\d+\b' | head -n 1`
+sudo kill -9 $pid
+
 sudo supervisord
 sudo supervisorctl start $nomeApp
